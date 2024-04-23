472756289d579ce4d062bb0c0e64df5054644cce
KajalMungroo <163845464+KajalMungroo@users.noreply.github.com>
2024-04-23 16:14:03 +0200

    Added weather API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
+function searchCity(city) {
+  let apiKey = "bb36b4b68f1o80faf1de69ed24adt430";
+  let apiUrl = `https:/api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
