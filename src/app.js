80445c29a3eacfd21b87d3621844df2117ca9393
KajalMungroo <163845464+KajalMungroo@users.noreply.github.com>
2024-04-23 17:10:53 +0200

    Added search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
   let apiKey = "bb36b4b68f1o80faf1de69ed24adt430";
-  let apiUrl = `https:/api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
